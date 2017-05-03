d3e28ad0613a577e43e2a1f8e68599545d7bb8d6
Emi Taylor <letailleur.emilie@gmail.com>
2017-05-03 19:20:06 +0200

    Ajustement des caroussels Nouveau fichier sql en vrac

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -4,6 +4,10 @@ namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Landing;
+use AppBundle\Entity\Roles;
+use AppBundle\Entity\Permissions;
+use AppBundle\Entity\Users;
 
 class DefaultController extends Controller
 {
--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -4,6 +4,10 @@ namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Landing;
+use AppBundle\Entity\Roles;
+use AppBundle\Entity\Permissions;
+use AppBundle\Entity\Users;
 
 class DefaultController extends Controller
 {
@@ -18,3 +22,4 @@ class DefaultController extends Controller
         return $this->render('UserBundle:Default:index.html.twig');
     }
 }
+        
